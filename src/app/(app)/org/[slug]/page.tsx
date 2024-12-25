c235da895da874dd82ddbfe2539266f352d5ea0d
Marcos Allan <marcosallanm1to@gmail.com>
2024-12-25 10:54:27 -0300

    Displaying active organization

---
+++ src/app/(app)/org/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from '@/components/header'
+
+export default async function Projects() {
+  return (
+    <div className="py-4">
+      <Header />
+      {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+      <main></main>
+    </div>
+  )
+}
