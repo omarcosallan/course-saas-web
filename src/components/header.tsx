2a025bf0d2b03b3e422144c55bd2c5814dc046d4
Marcos Allan <marcosallanm1to@gmail.com>
2024-12-25 11:36:25 -0300

    Dark/light themes with Next Themes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,6 +6,8 @@ import rocketseatIcon from '@/assets/rocketseat-icon.svg'
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 import { ability } from '@/auth/auth'
+import { Separator } from './ui/separator'
+import { ThemeSwitcher } from './theme/theme-switcher'
 
 export async function Header() {
   const permissions = await ability()
@@ -27,6 +29,8 @@ export async function Header() {
       </div>
 
       <div className="flex items-center gap-4">
+        <ThemeSwitcher />
+        <Separator orientation="vertical" className="h-5" />
         <ProfileButton />
       </div>
     </div>
