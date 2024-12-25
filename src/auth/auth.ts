cb9c4755dea91438d9d7af022c96289b6e34d723
Marcos Allan <marcosallanm1to@gmail.com>
2024-12-25 10:43:15 -0300

    Component: header

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -16,6 +16,7 @@ export async function auth() {
 
   try {
     const { user } = await getProfile()
+
     return { user }
   } catch {}
 
--- src/http/get-profile.ts
+++ src/http/get-profile.ts
@@ -11,6 +11,5 @@ interface GetProfileResponse {
 
 export async function getProfile() {
   const result = await api.get('profile').json<GetProfileResponse>()
-
   return result
 }
