2e0ef36909510ac5aa855ed9856e3330fb1030ca
Marcos Allan <marcosallanm1to@gmail.com>
2024-12-25 12:11:54 -0300

    Action: Criação de organização

---
+++ src/http/create-organization.ts
@@ -0,0 +1,26 @@
+import { api } from './api-client'
+
+interface CreateOrganizationRequest {
+  name: string
+  domain: string | null
+  shouldAttachUsersByDomain: boolean
+}
+
+// biome-ignore lint/suspicious/noConfusingVoidType: <explanation>
+type CreateOrganizationResponse = void
+
+export async function createOrganization({
+  name,
+  domain,
+  shouldAttachUsersByDomain,
+}: CreateOrganizationRequest): Promise<CreateOrganizationResponse> {
+  const result = await api.post('organizations', {
+    json: {
+      name,
+      domain,
+      shouldAttachUsersByDomain,
+    },
+  })
+
+  console.log(result)
+}
