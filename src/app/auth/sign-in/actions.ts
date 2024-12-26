dccf57a00b49f2d97cd348f706f8bf767792b1c8
Marcos Allan <marcosallanm1to@gmail.com>
2024-12-26 09:02:14 -0300

    Action: accept invitation

--- src/app/auth/sign-in/actions.ts
+++ src/app/auth/sign-in/actions.ts
@@ -1,10 +1,11 @@
 'use server'
 
 import { HTTPError } from 'ky'
+import { cookies } from 'next/headers'
 import { z } from 'zod'
 
+import { acceptInvite } from '@/http/accept-invite'
 import { signInWithPassword } from '@/http/sign-in-with-password'
-import { cookies } from 'next/headers'
 
 const signInSchema = z.object({
   email: z
@@ -35,6 +36,15 @@ export async function signInWithEmailAndPassword(data: FormData) {
       path: '/',
       maxAge: 60 * 60 * 24 * 7, // 7 days
     })
+
+    const inviteId = allCookies.get('inviteId')?.value
+
+    if (inviteId) {
+      try {
+        await acceptInvite(inviteId)
+        allCookies.delete('inviteId')
+      } catch {}
+    }
   } catch (err) {
     if (err instanceof HTTPError) {
       const { title } = await err.response.json()
